4ccb67b0519c2b011a45dcfe7a167f2b6cd2fc47
mityuk17 <mityukov.17@gmail.com>
2023-01-25 22:07:54 +0300

    need test

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import asyncio
 import os
 
-from opentele.api import API, UseCurrentSession
+from opentele.api import API , UseCurrentSession , CreateNewSession
 from telethon import TelegramClient
 from telethon.tl.functions.channels import JoinChannelRequest
-from telethon.tl.functions.messages import ImportChatInviteRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest , CheckChatInviteRequest
 
 
 def get_phrases():
-    with open('phrases.txt', 'r') as file:
+    with open('phrases.txt', 'r', encoding= 'utf8') as file:
         data = file.readlines()
     data = [i.strip() for i in data]
     return  data
@@ -21,38 +21,44 @@ def get_sessions():
     return subdir
 async def get_channel(client: TelegramClient):
     with open('channel_link.txt', 'r') as file:
-        data = file.readlines()[0].strip().strip('https://').strip('t.me/').strip('joinchat/')
+        data = file.readlines()[0].strip().strip('https://').strip('t.me/').strip('joinchat/').strip('+')
     try:
-        channel = await client.get_entity(data)
+        channel = (await client(CheckChatInviteRequest(data))).chat
     except Exception as e:
         print(e)
         channel = None
+    print(data,channel)
     return [data,channel]
 from opentele.td import TDesktop
 async def main():
 
     sessions = get_sessions()
     phrases = get_phrases()
+
     post = int(input("Введите ссылку на пост:").split('/')[-1])
 
     for session in sessions:
+        print(session)
         tdataFolder = f'sessions/{session}/tdata'
         tdesk = TDesktop(tdataFolder)
         client = await tdesk.ToTelethon(f'session_files/{session}',UseCurrentSession, api= API.TelegramAndroid.Generate(session))
         async with client:
             channel = await get_channel(client)
             try:
-                await client(JoinChannelRequest(channel[1]))
+                await client(JoinChannelRequest(channel[1].id))
             except Exception as e:
                 print(e)
                 try:
+
                     await client(ImportChatInviteRequest(channel[0]))
                 except Exception as e:
                     print(e)
-            msg = (await client.get_messages(channel[1].id, post))[0]
-            print(msg)
+            try:
+                msg = (await client.get_messages(channel[1], ids = post))
+            except ValueError:
+                msg = (await client.get_messages(channel[ 0] , ids=post))
             #await msg.reply(phrases.pop())
-            await client.send_message(entity=channel[0], message=phrases.pop(), comment_to=msg)
+            await client.send_message(entity=channel[1].id, message=phrases.pop(), comment_to=msg)
 def start():
 
     asyncio.run(main())
